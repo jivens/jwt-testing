764632a05f55a17dae73975235d32290f0a25206
John Ivens <john.wagner.ivens@gmail.com>
2019-06-05 11:47:41 -0700

    Initial script for json web tokens

---
+++ app.js
@@ -0,0 +1,40 @@
+'use strict';
+const fs   = require('fs');
+const jwt  = require('jsonwebtoken');
+
+// PAYLOAD
+var payload = {
+ data1: "Data 1",
+ data2: "Data 2",
+ data3: "Data 3",
+ data4: "Data 4",
+};
+
+// PRIVATE and PUBLIC key
+var privateKEY  = fs.readFileSync('./private.key', 'utf8');
+var publicKEY  = fs.readFileSync('./public.key', 'utf8');
+var i  = 'Mysoft corp';          // Issuer
+var s  = 'some@user.com';        // Subject
+var a  = 'http://mysoftcorp.in'; // Audience
+
+// SIGNING OPTIONS
+var signOptions = {
+ issuer:  i,
+ subject:  s,
+ audience:  a,
+ expiresIn:  "12h",
+ algorithm:  "RS256"
+};
+
+var token = jwt.sign(payload, privateKEY, signOptions);
+console.log("Token - " + token);
+
+var verifyOptions = {
+ issuer:  i,
+ subject:  s,
+ audience:  a,
+ expiresIn:  "12h",
+ algorithm:  ["RS256"]
+};
+var legit = jwt.verify(token, publicKEY, verifyOptions);
+console.log("\nJWT verification result: " + JSON.stringify(legit));
